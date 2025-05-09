9c6314e7709554d0a51b038a8f13f50f82d2275e
Sintayehu dev <sintayehubelfa23@gmail.com>
2025-05-09 04:42:38 -0700

    Initialize Go module and set up basic project structure with user model, authentication, and user routes. Add main application entry point and configure Gin router.

---
+++ routes/authRouter.go
@@ -0,0 +1,9 @@
+package routes
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func AuthRoutes(router *gin.Engine) {
+	// Auth routes will be defined here
+}
---
+++ routes/userRouter.go
@@ -0,0 +1,9 @@
+package routes
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func UserRoutes(router *gin.Engine) {
+	// User routes will be defined here
+}
